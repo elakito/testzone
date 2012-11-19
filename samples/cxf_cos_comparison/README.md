4507af0be0bde7ceaf34abd65a7d6cae0706dc5e
Aki Yoshida <elakito@server>
2012-11-19 10:31:39 +0100

    changed the test output format for cxf-cos-comaprison test
    Change-Id: I91eae8a1a66ff3893806848c0ef646e5718409e2

--- samples/cxf_cos_comparison/README.md
+++ samples/cxf_cos_comparison/README.md
@@ -13,39 +13,78 @@ is used to build and run the standalone unit test.
   
 Result with CXF 2.7.1-SNAPSHOT, i5 M540 2.53GHz, Win7
 ------------------------
-Running me.temp.samples.cxf.cos.test.CachedOutputStreamComparisonTest
-enc: none; roundtrip: 4096KB
-  time=105ms; 39MB/s
-enc: none; roundtrip: 16384KB
-  time=166ms; 98MB/s
-enc: none; roundtrip: 65536KB
-  time=524ms; 125MB/s
-enc: DES/CFB8/NoPadding; roundtrip: 4096KB
-  time=3206ms; 1MB/s
-enc: DES/CFB8/NoPadding; roundtrip: 16384KB
-  time=10661ms; 1MB/s
-enc: BLOWFISH; roundtrip: 4096KB
-  time=301ms; 13MB/s
-enc: BLOWFISH; roundtrip: 16384KB
-  time=781ms; 20MB/s
-enc: BLOWFISH; roundtrip: 65536KB
-  time=3174ms; 20MB/s
-enc: ARCFOUR; roundtrip: 4096KB
-  time=99ms; 41MB/s
-enc: ARCFOUR; roundtrip: 16384KB
-  time=348ms; 47MB/s
-enc: ARCFOUR; roundtrip: 65536KB
-  time=1444ms; 45MB/s
-enc: RC4; roundtrip: 4096KB
-  time=97ms; 42MB/s
-enc: RC4; roundtrip: 16384KB
-  time=357ms; 45MB/s
-enc: RC4; roundtrip: 65536KB
-  time=1473ms; 44MB/s
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 23.015 sec
 
-
-
-
-
-
+    -------------------------------------------------------
+     T E S T S
+    -------------------------------------------------------
+    Running me.temp.samples.cxf.cos.test.CachedOutputStreamComparisonTest
+    ### Supported Ciphers ###
+    cipherName
+    DES/CFB/PKCS5Padding
+    DES/ECB/PKCS5Padding
+    DES/CFB8/NoPadding
+    DES/CTR/NoPadding
+    BLOWFISH/CFB/NoPadding
+    BLOWFISH/ECB/PKCS5Padding
+    BLOWFISH/CFB8/NoPadding
+    BLOWFISH/CTR/NoPadding
+    AES/CBC/PKCS5Padding
+    AES/ECB/PKCS5Padding
+    AES/CFB/NoPadding
+    AES/CFB8/NoPadding
+    AES/CTR/NoPadding
+    RC4
+    ### WriteReadRoundTrip ###
+    cipherName;terminate;size[KB];time[ms];rate[MB/s]
+    none;false;4096;40;102
+    DES/CFB/PKCS5Padding;false;4096;573;7
+    DES/ECB/PKCS5Padding;false;4096;398;10
+    DES/CFB8/NoPadding;false;4096;2753;1
+    DES/CTR/NoPadding;false;4096;424;9
+    BLOWFISH/CFB/NoPadding;false;4096;321;12
+    BLOWFISH/ECB/PKCS5Padding;false;4096;232;17
+    BLOWFISH/CFB8/NoPadding;false;4096;1306;3
+    BLOWFISH/CTR/NoPadding;false;4096;242;16
+    AES/CBC/PKCS5Padding;false;4096;294;13
+    AES/ECB/PKCS5Padding;false;4096;177;23
+    AES/CFB/NoPadding;false;4096;256;16
+    AES/CFB8/NoPadding;false;4096;2054;1
+    AES/CTR/NoPadding;false;4096;204;20
+    RC4;false;4096;96;42
+    none;false;16384;150;109
+    DES/CFB/PKCS5Padding;false;16384;1555;10
+    DES/ECB/PKCS5Padding;false;16384;1477;11
+    DES/CFB8/NoPadding;false;16384;10741;1
+    DES/CTR/NoPadding;false;16384;1576;10
+    BLOWFISH/CFB/NoPadding;false;16384;877;18
+    BLOWFISH/ECB/PKCS5Padding;false;16384;806;20
+    BLOWFISH/CFB8/NoPadding;false;16384;5296;3
+    BLOWFISH/CTR/NoPadding;false;16384;921;17
+    AES/CBC/PKCS5Padding;false;16384;732;22
+    AES/ECB/PKCS5Padding;false;16384;1024;16
+    AES/CFB/NoPadding;false;16384;769;21
+    AES/CFB8/NoPadding;false;16384;8167;2
+    AES/CTR/NoPadding;false;16384;785;20
+    RC4;false;16384;384;42
+    ### WriteReadChunk ###
+    cipherName;terminate
+    none;false
+    DES/CFB/PKCS5Padding;true
+    DES/ECB/PKCS5Padding;true
+    DES/CFB8/NoPadding;false
+    DES/CTR/NoPadding;false
+    BLOWFISH/CFB/NoPadding;true
+    BLOWFISH/ECB/PKCS5Padding;true
+    BLOWFISH/CFB8/NoPadding;false
+    BLOWFISH/CTR/NoPadding;false
+    AES/CBC/PKCS5Padding;true
+    AES/ECB/PKCS5Padding;true
+    AES/CFB/NoPadding;true
+    AES/CFB8/NoPadding;false
+    AES/CTR/NoPadding;false
+    RC4;false
+    Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 45.894 sec
+    
+    Results :
+    
+    Tests run: 4, Failures: 0, Errors: 0, Skipped: 0
--- samples/cxf_cos_comparison/src/test/java/me/temp/samples/cxf/cos/test/CachedOutputStreamComparisonTest.java
+++ samples/cxf_cos_comparison/src/test/java/me/temp/samples/cxf/cos/test/CachedOutputStreamComparisonTest.java
@@ -20,7 +20,18 @@
 package me.temp.samples.cxf.cos.test;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.IvParameterSpec;
 
 import org.apache.cxf.io.CachedOutputStream;
 
@@ -32,6 +43,13 @@ public class CachedOutputStreamComparisonTest extends Assert {
     static final int TEST_INITIAL_SIZE = 1024;
     static final int TEST_UNIT_SIZE = 4096;
     static final byte[] TEST_UNIT_DATA;
+    static final String TEST_CHUNK_STRING = "Hello World! What a nice day."; 
+    static final byte[] TEST_CHUNK_BYTES = TEST_CHUNK_STRING.getBytes();
+    static final String[] CIPHER_NAMES = {null, "DES/CFB/PKCS5Padding", "DES/ECB/PKCS5Padding", "DES/CFB8/NoPadding",  "DES/CTR/NoPadding",
+                                          "BLOWFISH/CFB/NoPadding", "BLOWFISH/ECB/PKCS5Padding", "BLOWFISH/CFB8/NoPadding", "BLOWFISH/CTR/NoPadding",
+                                          "AES/CBC/PKCS5Padding", "AES/ECB/PKCS5Padding", "AES/CFB/NoPadding", "AES/CFB8/NoPadding", "AES/CTR/NoPadding",
+                                          "RC4"
+                                          };
 
     static {
         TEST_UNIT_DATA = new byte[TEST_UNIT_SIZE];
@@ -40,57 +58,190 @@ public class CachedOutputStreamComparisonTest extends Assert {
         }
     }
     
-    private long invokeWriteRead(int factor, String enc) throws Exception {
+    private long invokeWriteRead(int factor, String enc, boolean ftc) throws Exception {
         CachedOutputStream cos = new CachedOutputStream(TEST_THRESHOLD);
-        if (enc != null) {
-            cos.setCipherTransformation(enc);
-        }
-        byte[] buf = new byte[TEST_UNIT_SIZE];
-        long starttime = System.currentTimeMillis();
-        int size = TEST_INITIAL_SIZE * factor;
-        for (int i = 0; i < size; i++) {
-            cos.write(TEST_UNIT_DATA);
-        }
-        cos.flush();
-        InputStream in = cos.getInputStream();
-        int toread = size * TEST_UNIT_SIZE;
-        int offset = 0;
-        while (toread > 0) {
-            int n = in.read(buf, 0, buf.length);
-            if (n < 0) {
-                break;
+        try {
+            if (enc != null) {
+                cos.setCipherTransformation(enc);
             }
-            // verify the decoded data agains the original data
-            int nvfd = 0;
-            while (nvfd < n) {
-                if (buf[nvfd] != TEST_UNIT_DATA[(nvfd + offset) % TEST_UNIT_SIZE]) {
-                    fail("corrupted at " + nvfd + "-th byte in the buffer with reference at " 
-                        + (nvfd + offset) % TEST_UNIT_SIZE);
+            byte[] buf = new byte[TEST_UNIT_SIZE];
+            long starttime = System.currentTimeMillis();
+            int size = TEST_INITIAL_SIZE * factor;
+            for (int i = 0; i < size; i++) {
+                cos.write(TEST_UNIT_DATA);
+            }
+            cos.flush();
+            InputStream in = cos.getInputStream();
+            if (ftc) {
+                cos.close();
+            }
+
+            int toread = size * TEST_UNIT_SIZE;
+            int offset = 0;
+            try {
+                while (toread > 0) {
+                    int n = in.read(buf, 0, buf.length);
+                    if (n < 0) {
+                        break;
+                    }
+                    // verify the decoded data agains the original data
+                    int nvfd = 0;
+                    while (nvfd < n) {
+                        if (buf[nvfd] != TEST_UNIT_DATA[(nvfd + offset) % TEST_UNIT_SIZE]) {
+                            fail("corrupted at " + nvfd + "-th byte in the buffer with reference at " 
+                                + (nvfd + offset) % TEST_UNIT_SIZE);
+                        }
+                        nvfd++;
+                    }
+                    offset = (offset + n) % TEST_UNIT_SIZE; 
+                    toread -= n;
                 }
-                nvfd++;
+            } finally {
+                in.close();
             }
-            offset = (offset + n) % TEST_UNIT_SIZE; 
-            toread -= n;
+            long endtime = System.currentTimeMillis();
+            long total = (size * TEST_UNIT_SIZE) / 1024;
+            System.out.println(getCipherName(enc) + ";" + ftc + ";" + total + ";" + (endtime - starttime) + ";" + total / (endtime - starttime));
+            return endtime - starttime;
+        } finally {
+            if (!ftc) {
+                cos.close();
+            }
         }
-        long endtime = System.currentTimeMillis();
-        cos.close();
-        long total = (size * TEST_UNIT_SIZE) / 1024;
-        System.out.println("enc: " + (enc == null ? "none" : enc ) + "; roundtrip: " + total + "KB");
-        System.out.println("  time=" + (endtime - starttime) + "ms; " + total / (endtime - starttime) + "MB/s");
-        return endtime - starttime;
+    }
+    
+    private void invokeWriteReadChunk(String enc, boolean ftc) throws Exception {
+        CachedOutputStream cos = new CachedOutputStream(TEST_THRESHOLD);
+        try {
+            if (enc != null) {
+                cos.setCipherTransformation(enc);
+            }
+            // write the original test data
+            cos.write(TEST_CHUNK_BYTES);
+            cos.flush();
+            InputStream in = cos.getInputStream();
+            if (ftc) {
+                cos.close();
+            }
+
+            File tmpfile = cos.getTempFile();
+            assertNotNull("no file for cipher " + getCipherName(enc), tmpfile);
+            // verify the written raw data is not the same as the original
+            if (enc != null) {
+                assertFalse("not encodded by " + getCipherName(enc), TEST_CHUNK_STRING.equals(new String(getBytes(new FileInputStream(tmpfile)))));
+            }
+
+            // verify the decoded data
+            byte[] data = getBytes(in);
+            assertArrayEquals("corrupted by cipher " + getCipherName(enc) + " with ftc " + ftc +"; " + new String(TEST_CHUNK_BYTES) + " != " + new String(data),
+                              TEST_CHUNK_BYTES, data);
+            System.out.println(getCipherName(enc) + ";" + ftc);
+        } finally {
+            cos.close();
+        }
     }
 
-    private void repeatInvokeWriteRead(String enc) throws Exception {
-        for (int f = 1; invokeWriteRead(f, enc) < 5000 && f < 16; f <<= 2);
+    private static String getCipherName(String enc) {
+        return enc == null ? "none" : enc;
+    }
+    
+    private static byte[] getBytes(InputStream in) throws IOException {
+        byte[] buf = new byte[512];
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            for (;;) {
+                int n = in.read(buf, 0, buf.length);
+                if (n < 0) {
+                    break;
+                }
+                bos.write(buf, 0, n);
+            }
+            return bos.toByteArray();
+        } finally {
+            in.close();
+        }
+    }
+    
+    private static void initCipher(String name) throws GeneralSecurityException {
+        try {
+            int d = name.indexOf('/');
+            String a;
+            if (d > 0) {
+                a = name.substring(0, d);
+            } else {
+                a = name;
+            }
+            KeyGenerator keygen = KeyGenerator.getInstance(a);
+            keygen.init(new SecureRandom());
+            Key key = keygen.generateKey();
+            Cipher enccipher = Cipher.getInstance(name);
+            Cipher deccipher = Cipher.getInstance(name);
+            enccipher.init(Cipher.ENCRYPT_MODE, key);
+            final byte[] ivp = enccipher.getIV();
+            deccipher.init(Cipher.DECRYPT_MODE, key, ivp == null ? null : new IvParameterSpec(ivp));
+        } catch (GeneralSecurityException e) {
+            throw e;
+        }
+    }
+
+    @Test
+    public void testSupportedCiphers() throws Exception {
+        System.out.println("### Supported Ciphers ###");
+        System.out.println("cipherName");
+        for (String name : CIPHER_NAMES) {
+            if (name != null) {
+                try {
+                    initCipher(name);
+                    System.out.println(name);
+                } catch (Exception e) {
+                    fail("no such cipher: " + name);
+                }
+            }
+        }
+    }
+    @Test
+    public void testGerKeyGens() throws Exception {
+        KeyGenerator keygen;
+        keygen = KeyGenerator.getInstance("RC4");
+        keygen.init(new SecureRandom());
+        keygen = KeyGenerator.getInstance("RC4");
+        keygen.init(new SecureRandom());
+        keygen = KeyGenerator.getInstance("DES");
+        keygen.init(new SecureRandom());
+        keygen = KeyGenerator.getInstance("DES");
+        keygen.init(new SecureRandom());
+        keygen = KeyGenerator.getInstance("AES");
+        keygen.init(new SecureRandom());
+        keygen = KeyGenerator.getInstance("AES");
+        keygen.init(new SecureRandom());
+    }
+    @Test
+    public void testWriteReadRoundtripWithCipher() throws Exception {
+        System.out.println("### WriteReadRoundTrip ###");
+        System.out.println("cipherName;terminate;size[KB];time[ms];rate[MB/s]");
+        for (int f = 1; f < 8; f <<= 2) {
+            for (String name : CIPHER_NAMES) {
+                try {
+                    invokeWriteRead(f, name, false);
+                } catch (AssertionError ae) {
+                    // see if the error goes away when the cipher is closed
+                    invokeWriteRead(f, name, true);
+                }
+            }
+        }
     }
     
     @Test
-    public void testWriteReadRoundtripWithCipher() throws Exception {
-        repeatInvokeWriteRead(null);
-        repeatInvokeWriteRead("DES/CFB8/NoPadding");
-        repeatInvokeWriteRead("BLOWFISH");
-        repeatInvokeWriteRead("ARCFOUR");
-        repeatInvokeWriteRead("RC4");
+    public void testWriteReadChunkWithCipher() throws Exception {
+        System.out.println("### WriteReadChunk ###");
+        System.out.println("cipherName;terminate");
+        for (String name : CIPHER_NAMES) {
+            try {
+                invokeWriteReadChunk(name, false);
+            } catch (AssertionError ae) {
+                // see if the error goes away when the cipher is closed
+                invokeWriteReadChunk(name, true);
+            }
+        }
     }
-
 }
